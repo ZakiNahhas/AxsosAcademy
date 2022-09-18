5ea735ef2cd0e10c9c7a9cd05373e9c1cfbb55ec
Zaki <zakinahhas97@outlook.com>
2022-09-18 13:13:30 +0300

    added a new assignment

---
+++ python_stack/flask/flask_fundamentals/hello_flask/tableServer.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__) 
+
+@app.route('/table')
+def table():
+    myTable = [
+        {'First Name': "Khaled", 'Last Name': "Ammar"},
+        {'First Name': "Zaki", 'Last Name': "Nahhas"},
+        {'First Name': "Yousef", 'Last Name': "Assi"}
+    ]
+    return render_template("table.html", tableinfo=myTable, table=myTable[0])
+
+if __name__=="__main__":  
+    app.run(debug=True)  
