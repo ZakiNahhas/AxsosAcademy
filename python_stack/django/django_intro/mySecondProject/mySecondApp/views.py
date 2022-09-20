61a325f3f1e6795eb6910981833812f40673ee33
Zaki <zakinahhas97@outlook.com>
2022-09-20 14:41:18 +0300

    added a new assignment

---
+++ python_stack/django/django_intro/mySecondProject/mySecondApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+from time import gmtime, strftime
+
+def time(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request,'index.html', context)
+
+def display(request):
+    return redirect('/')
