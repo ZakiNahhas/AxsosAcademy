e9c9f9c188ef1285977a391fc09048c4f811fe56
Zaki <zakinahhas97@outlook.com>
2022-10-24 11:20:53 +0300

    java funds

---
+++ javaStack/javaFundementals/Me.java
@@ -0,0 +1,7 @@
+public class Me {
+    public static void main(String[] args) {
+        System.out.println("My name is Zaki Nahhas");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Al-Bireh");
+    }
+}
