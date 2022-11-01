ed154bd0fcca4a8b1bf5fa91a315542deb97b18e
Zaki <zakinahhas97@outlook.com>
2022-11-01 16:10:27 +0200

    Added an assignment

---
+++ javaStack/springProjects/FruitsLoop/src/main/java/com/zaki/fruitsloop/FruitsLoopApplication.java
@@ -0,0 +1,12 @@
+package com.zaki.fruitsloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsLoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitsLoopApplication.class, args);
+    }
+}
---
+++ javaStack/springProjects/FruitsLoop/src/test/java/com/zaki/fruitsloop/FruitsLoopApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zaki.fruitsloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsLoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
