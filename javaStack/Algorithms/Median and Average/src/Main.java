9cb82d31e2ea6abd10fc384586b406d8d36f41bd
Zaki <zakinahhas97@outlook.com>
2022-11-01 13:23:08 +0200

    Added an assignment

--- javaStack/Algorithms/Median and Average/src/Main.java
+++ javaStack/Algorithms/Median and Average/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         MedianAvg medianAvg = new MedianAvg();
-        System.out.println(medianAvg.medianAverage([1,2,3], [3,4,5]));
+        int[] array1 = {1,2,3};
+        int [] array2 = {3,4};
+        System.out.println(medianAvg.medianAverage(array1, array2));
     }
 }
