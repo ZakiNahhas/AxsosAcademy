439d3e28246fe68648b7daa3de76b897a623c35e
Zaki <zakinahhas97@outlook.com>
2022-11-09 12:52:08 +0200

    Added an assignment

---
+++ javaStack/springProjects/LoginAndRegistration/src/main/java/com/zaki/loginandregistration/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.zaki.loginandregistration.repositories;
+
+import com.zaki.loginandregistration.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
---
+++ javaStack/springProjects/LoginAndRegistration/src/test/java/com/zaki/loginandregistration/LoginAndLoginApplicationTestsUser.java
@@ -0,0 +1,12 @@
+package com.zaki.loginandregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndLoginApplicationTestsUser {
+
+    @Test
+    void contextLoads() {
+    }
+}
