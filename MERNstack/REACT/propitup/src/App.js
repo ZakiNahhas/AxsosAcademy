04de721a23802c74d860d633b822d6dc1dde1e45
Zaki <zakinahhas97@outlook.com>
2022-12-07 15:31:03 +0200

    React Assignments

--- MERNstack/REACT/propitup/src/App.js
+++ MERNstack/REACT/propitup/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Propitup name={"Zaki Nahhas"} age = {25} hairColor={"brown"}/>
       <Propitup name={"Izz Malakh"} age = {23} hairColor={"black"}/>
       <Propitup name={"Haninz Saymeh"} age = {25} hairColor={"red"}/>
-      <Propitup name={"Sponge Bob"} age = {155} hairColor={"yellow"}/>
+      <Propitup name={"Sponge Bob"} age = {155} hairColor={"yellow"}/> 
     </div>
   );
 }
--- MERNstack/REACT/propitup/src/components/Propitup.jsx
+++ MERNstack/REACT/propitup/src/components/Propitup.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 export default class Propitup extends Component {
-  render(props) {
+    constructor(props) {
+        super(props);
+        this.state = {
+          counter: this.props.age,
+        };
+    }
+    fun1 = ()=> {
+        this.setState({counter:this.state.counter+1})
+    }
+    fun2 = ()=>{
+        this.setState({counter:this.props.age})
+    }
+  render() {
     return (
+        
       <div className='margin'>
         <h1>{this.props.name}</h1>
-        <p>Age: {this.props.age}</p>
+        <p>Age: {this.state.counter}</p>
         <p>Hair color: {this.props.hairColor}</p>
+        <button id="b" className='btn btn-success' onClick={this.fun1}>Birthday button for {this.props.name}</button>
+        <button className='btn btn-danger'  onClick={this.fun2}>Reset</button>
       </div>
     )
   }
